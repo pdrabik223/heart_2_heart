c7aebc59298afaaa3100b6dc52af74cb1de8a53e
pdrabik223 <pdrabik223@gmail.com>
2025-08-20 21:11:52 +0200

    fix: update static file serving path to include current directory

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './')));
 
 app.listen(PORT, () => {
   console.log(`Heart 2 Heart app running at http://localhost:${PORT}`);
